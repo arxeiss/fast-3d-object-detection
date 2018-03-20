0b4f560b803a3f93011e900012ec3d7de3324855
Pavel Kutáč <pavel.kutac@gmail.com>
2018-03-20 23:13:11 +0100

    Add Tessil implementation of unordered_map (hopscotch_map)

--- Fast3DObjectDetection/constantsAndTypes.h
+++ Fast3DObjectDetection/constantsAndTypes.h
@@ -5,7 +5,7 @@
 	#define _USE_MATH_DEFINES
 #endif // !_USE_MATH_DEFINES
 #include <math.h>
-#include <unordered_map>
+#include "hopscotch_map.h"
 
 #include "DetectionUnit.h"
 #include "QuantizedTripletValues.h"
@@ -46,4 +46,4 @@ const cv::Mat yDerivMask = (cv::Mat_<float>(3, 3) << -1.0f, -2.0f, -1.0f, 0, 0, 
 
 typedef std::vector<DetectionUnit> TemplateList;
 typedef std::vector<TemplateList> FolderTemplateList;
-typedef std::unordered_map<QuantizedTripletValues, std::vector<TemplateIndex>> TemplateHashTable;
+typedef tsl::hopscotch_map<QuantizedTripletValues, std::vector<TemplateIndex>> TemplateHashTable;
